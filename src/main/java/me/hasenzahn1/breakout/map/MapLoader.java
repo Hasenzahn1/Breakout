46362943117dde043c4971f506638a94b60019a2
Hasenzahn1 <motzer10@gmx.de>
2022-07-05 16:26:54 +0200

    Define Structure of Map System
    Anwesende Personen:
     - Benedikt
     - Fabian

---
+++ src/main/java/me/hasenzahn1/breakout/map/MapLoader.java
@@ -0,0 +1,16 @@
+package me.hasenzahn1.breakout.map;
+
+import java.util.ArrayList;
+
+public class MapLoader {
+
+    public static ArrayList<String> getMaps(){
+        return new ArrayList<>();
+    }
+
+    public static Map loadMap(String name){
+
+        return null;
+    }
+
+}
---
+++ src/main/java/me/hasenzahn1/breakout/map/bricks/UnbreakableBrick.java
@@ -0,0 +1,16 @@
+package me.hasenzahn1.breakout.map.bricks;
+
+import java.awt.*;
+
+public class UnbreakableBrick extends Brick{
+
+    @Override
+    public void render(Graphics g) {
+        super.render(g);
+    }
+
+    @Override
+    public void tick(double deltaTime) {
+        super.tick(deltaTime);
+    }
+}
