a5a2db0a40b1877c2189b20b0915fe9462626f80
Sylencer <cykablyat1706@gmail.com>
2022-07-06 15:17:24 +0200

    Vec2d working
    Present people:
    -Benedikt

--- src/main/java/me/hasenzahn1/breakout/math/Vec2d.java
+++ src/main/java/me/hasenzahn1/breakout/math/Vec2d.java
@@ -10,5 +10,29 @@ public class Vec2d{
         this.n2 = n2;
     }
 
+    public Vec2d normalize(){
+        double length = Math.sqrt((n1*n1)+(n2*n2));
+        n1 = n1/length;
+        n2 = n2/length;
+        return this;
+    }
+    public Vec2d multiply(double x){
+        n1 *= x;
+        n2 *= x;
+        return this;
+    }
+    
+    public Vec2d add(double a, double b){
+        n1 += a;
+        n2 += b;
+        return this;
+    }
+    
+    public void setN1(double n1) {
+        this.n1 = n1;
+    }
 
+    public void setN2(double n2) {
+        this.n2 = n2;
+    }
 }
