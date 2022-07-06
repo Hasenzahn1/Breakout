20fa776323e575580a429ab128ad9e1b375bd06c
Sylencer <cykablyat1706@gmail.com>
2022-07-06 15:27:20 +0200

    Paddle movement methods
    Present people:
    -Benedikt

--- src/main/java/me/hasenzahn1/breakout/game/Paddle.java
+++ src/main/java/me/hasenzahn1/breakout/game/Paddle.java
@@ -1,10 +1,13 @@
 package me.hasenzahn1.breakout.game;
 
+import me.hasenzahn1.breakout.display.IDrawable;
+import me.hasenzahn1.breakout.image.ImageLoader;
 import me.hasenzahn1.breakout.math.BoundingBox;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class Paddle implements ICollidable{
+public class Paddle implements ICollidable, IDrawable {
 
 
     private float x, y;
@@ -19,6 +22,24 @@ public class Paddle implements ICollidable{
     @Override
     public void onCollide(Ball ball) {
 
+    }
+
+    public void moveTo(float newX){
+        x = newX;
+    }
+
+    public void move(float sub){
+        x += sub;
+    }
+
+    @Override
+    public void tick(double deltaTime) {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(image, (int)x, (int)y, width, image.getHeight(),null);
     }
 }
 
